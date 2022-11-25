05eefd8e548ef46ddc0708d47a1d43a5a3b0351a
ankita007-coder <ankitabudhia42@gmail.com>
2022-11-25 23:23:33 +0530

    Firebase added successfully

--- src/App.js
+++ src/App.js
@@ -1,59 +1,101 @@
 import React from 'react';
 import Cart from './Cart';
 import Navbar from './Navbar';
-
+import firebase from 'firebase/compat/app';
+import 'firebase/firestore';
+import {} from 'firebase/compat/firestore';
+//import {} from 'firebase/firestore';
 
 class App extends React.Component {
   constructor(){
     super();
     this.state = {
-        products: [
-            {
-                price: 999,
-                title: 'Phone',
-                qty: 1,
-                img: 'https://images.unsplash.com/photo-1565849904461-04a58ad377e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=436&q=80',
-                id: 1
-            },
-            {
-                price: 999,
-                title: 'Watch',
-                qty: 1,
-                img: 'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80',
-                id: 2
-            },
-            {
-                price: 999,
-                title: 'Laptop',
-                qty: 1,
-                img: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
-                id: 3
-            }
-            
-        ]
+        products: [],
+        loading: true
     }
+    this.db = firebase.firestore();
+}
+componentDidMount(){
+  // firebase
+  // .firestore()
+  // .collection('products')
+  // .get()
+  // .then((snapshot)=>{
+  //   console.log(snapshot);
+  //   snapshot.docs.map((doc)=>{
+  //     console.log(doc.data());
+  //   });
+  //   const products=snapshot.docs.map((doc)=>{
+  //     const data = doc.data();
+  //     data['id'] = doc.id;
+  //     return data;
+  //   });
+  //   this.setState({
+  //     products,
+  //     loading: false
+  //   });
+  // })
+  this.db
+  .collection('products')
+  .onSnapshot((snapshot)=>{
+    console.log(snapshot);
+    snapshot.docs.map((doc)=>{
+      console.log(doc.data());
+    });
+    const products=snapshot.docs.map((doc)=>{
+      const data = doc.data();
+      data['id'] = doc.id;
+      return data;
+    });
+    this.setState({
+      products,
+      loading: false
+    });
+  })
 }
 handleIncreaseQuantity =(product)=>{
-    const {products} = this.state;
-    const ind = products.indexOf(product);
-    products[ind].qty+=1;
-    this.setState({
-        products
+  const {products} = this.state;
+  const ind = products.indexOf(product);
+//   products[ind].qty+=1
+// this.setState({
+//   products
+// })
+  const docRef = this.db.collection('products').doc(products[ind].id);
+  docRef
+    .update({
+      qty: products[ind].qty+1
+    })
+    .then(() =>{
+      console.log('Updated product');
+    })
+    .catch(err =>{
+      console.log('Error updating product',err);
     })
 }
 handleDecreaseQuantity= (product)=>{
     const {products} = this.state;
     const ind = products.indexOf(product);
-    if (products[ind].qty>0){
-        products[ind].qty-=1
-    
+    if (products[ind].qty===0){
+      return ;
+    }
+    const docRef = this.db.collection('products').doc(products[ind].id);
+    docRef
+       .update({
+        qty: products[ind].qty-1
+  })
+      .then(()=>{
+        console.log('updated product');
+      })
+      .catch(err=>{
+        console.log('error updating product',err);
+      });
+
 }
-this.setState({
-    products
-})
+// this.setState({
+//     products
+// })
     
    
-}
 handleDeleteQuantity = (id)=>{
 const{products} = this.state;
 const items = products.filter
@@ -78,11 +120,30 @@ getTotal = ()=>{
   });
   return total;
 }
+
+addProduct=()=>{
+  this.db
+  .collection('products')
+  .add({
+    img: '',
+    price: 99999,
+    qty: 3,
+    title: 'Washing MAchine'
+  })
+  .then((docRef)=>{
+    console.log(docRef);
+  })
+  .catch((err)=>{
+    console.log(err);
+  });
+}
+
 render(){
-  const {products} = this.state;
+  const {products,loading} = this.state;
   return (
     <div className="App">
       <Navbar count = {this.getCount()}/>
+      {/* <button onClick={this.addProduct} style={{fontSize: 12, padding:20}}>Add a product</button> */}
       <Cart
           products = {products}
           increaseQuantity={this.handleIncreaseQuantity}
@@ -90,6 +151,7 @@ render(){
           deleteQuantity = {this.handleDeleteQuantity} 
       />
       <div style={{padding:10, fontSize: 20}}>TOTAL : {this.getTotal()}</div>
+      {loading && <h1>Loading...</h1>}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// v9 compat packages are API compatible with v8 code
+//import { initializeApp } from 'firebase/app';
+//import { getFirestore } from 'firebase/firestore';
+import firebase from 'firebase/compat/app';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA_j9vinNLt3MfdLTkVBNa3yeBYCaV9BXo",
+  authDomain: "cart-29843.firebaseapp.com",
+  projectId: "cart-29843",
+  storageBucket: "cart-29843.appspot.com",
+  messagingSenderId: "573256362707",
+  appId: "1:573256362707:web:99589ca2667e60782ec226"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
